0ea52e47825cd8d520f50d39abcddae5b18a4cc3
brucekomike <brucekomike@gmail.com>
2024-04-24 09:49:06 +0800

    make regex works

--- src/60-nginx.sh
+++ src/60-nginx.sh
@@ -8,9 +8,10 @@ new_domain=$DOMAIN
 export nginx_dir="/etc/nginx/sites-available/*"
 export nginx_enable_dir="/etc/nginx/sites-enabled/"
 # replace example.con with your setting in 00-env.sh
-for config_file in 
+for config_file in $nginx_dir
 do
 # use sed to replace old domain with new domain in nginx config file
-sed -ir "/server_name/ { n; s/$old_name/$new_name/g; }" "$config_file"
-ln -s $config_file $nginx_enable_dir
+export regex="s/\(server_name[^*]*\)$old_domain\([^;]*;\)/\1$new_domain\2/g"
+sed -ir $regex $config_file
+#ln -s $config_file $nginx_enable_dir
 done
